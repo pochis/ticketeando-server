c1f912845dcf8499a8263e8f80b1417dba0f9b04
pochis852 <pochis43@yahoo.es>
2018-02-15 15:43:21 +0000

    MASTER CORS

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,10 +15,21 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $headers = [
+            'Content-type' => 'application/json;charset=UTF-8',
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age' => '86400',
+            'Access-Control-Allow-Headers' => $request->header('Access-Control-Request-Headers')
+        ];
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
         $response = $next($request);
-        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
-        $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        $response->header('Access-Control-Allow-Origin', '*');
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
         return $response;
     }
 }
